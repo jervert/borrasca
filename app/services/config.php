f961489f19240f797c2c097cc958e9825d7c58a1
Antonio Rodríguez <antoniorodriguezruiz@gmail.com>
2013-11-18 23:50:12 +0100

    moved config

--- app/services/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-$config['openweathermaps_apikey'] = '113f7769a2570b6e4151869ab5d205d7 ';
-?>
---
+++ php/config.php
@@ -0,0 +1,4 @@
+<?php
+$config['openweathermaps_apikey'] = '113f7769a2570b6e4151869ab5d205d7 ';
+$config['data_origin'] = 'real'; // 'real' (for real data), mock (for example data)
+?>
