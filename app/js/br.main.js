45ad043f33975e49549db722cb9885f1b71f334a
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2013-11-27 08:45:56 +0100

    avoid detail json caching in browser

--- app/js/br.main.js
+++ app/js/br.main.js
@@ -103,7 +103,7 @@ var $Q, $, Globalize, _, Backbone, Highcharts, L;
   $Q.services = {
     main: ($Q.server === 'php') ? $Q.servicePath + 'services/main.php?lang=' + $Q.alternateCulture + '&geolocation=' : $Q.servicePath + '?lang=' + $Q.alternateCulture + '&geolocation=',
     search: ($Q.server === 'php') ? $Q.servicePath + 'services/search.php?location_name=' : $Q.servicePath + '?location_name=',
-    detail: ($Q.server === 'php') ? $Q.servicePath + 'services/detail.php?lang=' + $Q.alternateCulture + '&location_name=' : $Q.servicePath + '?detail=true&lang=' + $Q.alternateCulture + '&location_name=',
+    detail: ($Q.server === 'php') ? $Q.servicePath + 'services/detail.php?lang=' + $Q.alternateCulture + '&location_name=' : $Q.servicePath + '?detail=true&lang=' + $Q.alternateCulture + '&version=' + Date.now() + '&location_name=',
     xml: ($Q.server === 'php') ? $Q.servicePath + 'services/xml.php?location_id=' : $Q.servicePath + '?xml='
   }
 
