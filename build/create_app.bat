4566800506ceed09738323a36a4ec0891315efe0
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2013-12-15 21:59:09 +0100

    build scripts update

--- build/create_app.bat
+++ build/create_app.bat
@@ -1,15 +1,17 @@
 @echo off
 
-set appName=Borrasca-Next
-set appId=com.phonegap.borrasca_v2
-set workspace=%ADT_WORKSPACE%/borrasca-next
+set APPNAME=Borrasca-Next
+set APPID=com.phonegap.borrasca_v2
+set LETTERDRIVE=d:
+set ADT_WORKSPACE=D:\Users\antonio\Desktop\WEB\WORKSPACE
+set WORKSPACE=%ADT_WORKSPACE%/borrasca-next
 
 
-call phonegap create %workspace% -n $appName -i $appId
+call phonegap create %WORKSPACE% -n %APPNAME% -i %APPID%
 echo Created app
-call %letterDrive%
-cd %workspace%
-echo Moved to %workspace%
+call %LETTERDRIVE%
+cd %WORKSPACE%
+echo Moved to %WORKSPACE%
 call cordova platform add android
 call cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-geolocation.git
 call cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-file.git
