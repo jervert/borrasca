8c1bd6c8ed70d376381e816ed06baacfae6bd187
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2014-01-17 23:30:36 +0100

    moved assets adjustment

--- compass/config.rb
+++ compass/config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/app"
-css_dir = "../app/css"
+css_dir = "../app/assets/css"
 sass_dir = "sass"
-images_dir = "../app/css"
-javascripts_dir = "../app/css"
+images_dir = "../app/assets/css"
+javascripts_dir = "../app/assets/css"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
