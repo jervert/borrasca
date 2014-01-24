8e223acf57b9244079ebc1018b537e0646be0047
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2014-01-24 18:18:22 +0100

    home wip

--- app/assets/js/mvc/home.mvc.js
+++ app/assets/js/mvc/home.mvc.js
@@ -2,6 +2,7 @@
   $Q.HomeView = $Q.extendView.extend({
     el: '#page-area',
     tmpl: '#tmpl-home',
+    searchResults: ''
     events: {
       'click [data-link]': 'navigate'
     },
@@ -19,6 +20,19 @@
       $(document.body).addClass('home');
       this.setStorageLocationsToModel();
     },
-    afterRender: function () {}
+    afterRender: function () {},
+    doSearch: function (ev) {
+      var self = this;
+      this.$el.find(this.searchResults).html(_.c_loading());
+      this.getModelUrl();
+      this.model.fetch({
+        success: function (model, response) {
+          //self.render();
+        },
+        error: function () {
+          self.$el.find(self.searchResults).html(_.c_message_error());
+        }
+      });
+    },
   });
 }());
