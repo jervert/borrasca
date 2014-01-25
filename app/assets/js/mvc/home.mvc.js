d263b47c976225d092689037e449e51dc03d0ea5
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2014-01-25 01:43:02 +0100

    search in home page

--- app/assets/js/mvc/home.mvc.js
+++ app/assets/js/mvc/home.mvc.js
@@ -2,9 +2,13 @@
   $Q.HomeView = $Q.extendView.extend({
     el: '#page-area',
     tmpl: '#tmpl-home',
-    searchResults: ''
+    searchResults: '#results-area',
+    searchResultsTmpl: '#tmpl-home-search-results',
+    searchInput: '#c-home-search',
     events: {
-      'click [data-link]': 'navigate'
+      'click [data-link]': 'navigate',
+      'submit [data-search-form]': 'doSearch',
+      'click [data-search-button]': 'doSearch'
     },
     afterInitialize: function () {
       var self = this;
@@ -21,13 +25,17 @@
       this.setStorageLocationsToModel();
     },
     afterRender: function () {},
+    getModelUrl: function () {
+      this.model.url = $Q.services.search + $(this.searchInput).val() + _.avoidCacheParam();
+    },
     doSearch: function (ev) {
+      ev.preventDefault();
       var self = this;
       this.$el.find(this.searchResults).html(_.c_loading());
       this.getModelUrl();
       this.model.fetch({
         success: function (model, response) {
-          //self.render();
+          self.$el.find(self.searchResults).html(_.tmpl(self.searchResultsTmpl, {locations: self.model.get('locations')}));
         },
         error: function () {
           self.$el.find(self.searchResults).html(_.c_message_error());
