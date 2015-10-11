917ccb68d858461ccf287b7200dbbbed2e92a90e
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2015-10-12 00:56:07 +0200

    update sliceslide

--- app/assets/js/libs/slice-slide/jquery.sliceslide-4.0.js
+++ app/assets/js/libs/slice-slide/jquery.sliceslide-4.0.js
@@ -1,7 +1,7 @@
 /*
  * SliceSlide - jQuery and Underscore plugin for slideshows
  *
- * Copyright (c) 2012 Antonio Rodriguez Ruiz
+ * Copyright (c) 2015 Antonio Rodriguez Ruiz
  *
  * Licensed under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
@@ -9,17 +9,13 @@
  * Project home:
  *   http://outbook.es
  *
- * Version:  4.1.1
+ * Version:  4.2.1
  *
  */
 
 
 (function($) {
   $.fn.sliceSlide = function (options, extendedFn) {
-    _.templateSettings = {
-      interpolate: /\{\{(.+?)\}\}/g,
-      evaluate: /\[\[(.+?)\]\]/g
-    };
     var fnBase, defaults = {
       slidesBox: '[data-slice-slide-box]',
       slidesBoxSlide: '[data-slice-slide]',
@@ -50,12 +46,20 @@
       effectTime: 150,
       autoStart: true,
       loop: true,
+      controlsAdditionOrder: 'prepend',
       templatesUrl: 'jquery.sliceslide.templates.html',
       templatesCultureUrl: 'sliceslide_cultures/jquery.sliceslide.##CULTURE##.json',
       idSliceSlideTemplates: 'jquery-slice-slide-templates',
+      slideTemplatesPlaceholder: true,
+      setTemplateSettings: true,
+      templateSettings: {
+        interpolate: /\{\{(.+?)\}\}/g,
+        evaluate: /\[\[(.+?)\]\]/g
+      },
       slideTime: 3,
       culture: $('html').attr('lang') || 'en'
     },
+      self,
       op = $.extend(true, {}, defaults, options),
 
       defaultFn = {
@@ -70,19 +74,29 @@
           left: 37
         },
         init: function () {
-          var self = this;
+          self = this;
           self.op.numberSimultaneousSlides = (self.op.thumbnailsInControls) ? 1 : self.op.numberSimultaneousSlides;
           self.el.slides = self.el.slidesBox.find(op.slidesBoxSlide);
-          self.el.idSlideBox = this.getSlidesBoxId();
+          self.el.idSlideBox = self.getSlidesBoxId();
           self.el.slidesBox.attr('id', self.el.idSlideBox);
-          self.ariaSlideBox();
-          self.ariaSlides();
+          self.setTemplateSettings();
+          self.initAria();
           self.setSlides(); // Activate initial slides, and hide all other
           self.controls(); // Play-Pause and slide number controls
         },
 
+        initAria: function () {
+          self.ariaSlideBox();
+          self.ariaSlides();
+        },
+
+        setTemplateSettings: function () {
+          if (op.setTemplateSettings) {
+            _.templateSettings = op.templateSettings;
+          }
+        },
+
         ariaSlideBox: function () {
-          var self = this;
           if (!self.el.slidesBox.is('[role="listbox"]')) {
             self.el.slidesBox.attr('role', 'listbox');
           }
@@ -99,13 +113,12 @@
           element.attr(attributes);
         },
         ariaSlides: function () {
-          var self = this;
           $.each(self.el.slides, function (index, slideObject) {
             var slide = $(slideObject);
             self.addAriaAttributes(slide);
-            slide.on('keydown', function (event) {
-              if (event.keyCode === self.keys.left || event.keyCode === self.keys.up) {
-                event.preventDefault();
+            slide.on('keydown', function (ev) {
+              if (ev.keyCode === self.keys.left || ev.keyCode === self.keys.up) {
+                ev.preventDefault();
                 self.pauseSlide();
                 if (slide.prev(op.slidesBoxSlideActive).length > 0) {
                   slide.prev(op.slidesBoxSlideActive).focus();
@@ -114,8 +127,8 @@
                 }
               }
 
-              if (event.keyCode === self.keys.right || event.keyCode === self.keys.down) {
-                event.preventDefault();
+              if (ev.keyCode === self.keys.right || ev.keyCode === self.keys.down) {
+                ev.preventDefault();
                 self.pauseSlide();
                 if (slide.next(op.slidesBoxSlideActive).length > 0) {
                   slide.next(op.slidesBoxSlideActive).focus();
@@ -128,24 +141,22 @@
         },
 
         ariaControlsFixed: function () {
-          var self = this;
-          self.el.fixedLinks.on('keydown', function (event) {
-            //console.log(event.keyCode);
+          self.el.fixedLinks.on('keydown', function (ev) {
             var controlContainer = $(this).closest(op.slidesBoxControlsFixed),
               controlContainerPrev = controlContainer.prev(op.slidesBoxControlsFixed),
               controlContainerNext = controlContainer.next(op.slidesBoxControlsFixed),
               controlContainerSiblings = controlContainer.siblings(op.slidesBoxControlsFixed);
 
-            if (event.shiftKey && event.keyCode === self.keys.tab) {
-              event.preventDefault();
+            if (ev.shiftKey && ev.keyCode === self.keys.tab) {
+              ev.preventDefault();
               self.el.slideControlsBox.find(op.slidesBoxControlsPrevAndNext).first().find(op.links).first().focus();
-            } else if (!event.shiftKey && event.keyCode === self.keys.tab) {
-              event.preventDefault();
+            } else if (!ev.shiftKey && ev.keyCode === self.keys.tab) {
+              ev.preventDefault();
               self.el.slides.filter('[aria-selected="true"]').first().focus();
             }
 
-            if (event.keyCode === self.keys.left || event.keyCode === self.keys.up) {
-              event.preventDefault();
+            if (ev.keyCode === self.keys.left || ev.keyCode === self.keys.up) {
+              ev.preventDefault();
               if (controlContainerPrev.length > 0) {
                 controlContainerPrev.find(op.links).first().focus();
               } else {
@@ -153,8 +164,8 @@
               }
             }
 
-            if (event.keyCode === self.keys.right || event.keyCode === self.keys.down) {
-              event.preventDefault();
+            if (ev.keyCode === self.keys.right || ev.keyCode === self.keys.down) {
+              ev.preventDefault();
               if (controlContainerNext.length > 0) {
                 controlContainerNext.find(op.links).first().focus();
               } else {
@@ -165,8 +176,7 @@
         },
 
         getSlidesBoxId: function () {
-          var self = this,
-            idSlideBox = op.prefixId + (self.el.slidesBoxIndex + 1);
+          var idSlideBox = op.prefixId + (self.el.slidesBoxIndex + 1);
           while ($('#' + idSlideBox).length) {
             self.el.slidesBoxIndex += 1;
             idSlideBox = op.prefixId + (self.el.slidesBoxIndex + 1);
@@ -175,12 +185,11 @@
         },
 
         tmpl: function (id, context) {
-          var html = $(id).html();
-          return _.template(html, context);
+          var html = _.template($(id).html());
+          return html(context);
         },
 
         setSlides: function () {
-          var self = this;
           self.el.slides.each(function (index, slide) {
             $(slide).attr('id', self.el.idSlideBox + '-' + (index + 1));
             if (op.translucentElement) {
@@ -191,8 +200,7 @@
         },
 
         initialSlides: function () {
-          var self = this,
-            allSlides = self.el.slidesBox.find(op.slidesBoxSlide),
+          var allSlides = self.el.slidesBox.find(op.slidesBoxSlide),
             initialSlides = allSlides.filter(':lt(' + op.numberSimultaneousSlides + ')'),
             notInitialSlides = allSlides.filter(':gt(' + (op.numberSimultaneousSlides - 1) + ')');
           initialSlides.addClass(op.classesActive);
@@ -200,8 +208,7 @@
         },
 
         getThumbnailsRoutes: function () {
-          var self = this,
-            thumbnails = [];
+          var thumbnails = [];
           self.el.slides.each(function () {
             thumbnails.push($(this).attr(self.op.attrThumb));
           });
@@ -209,22 +216,19 @@
         },
 
         controls: function () {
-          var self = this,
-            pagesNumberRaw = self.el.slides.length / self.op.numberSimultaneousSlides, //
+          var pagesNumberRaw = (self.el.slides.length) / (self.op.numberSimultaneousSlides),
             pagesNumber = Math.ceil(pagesNumberRaw),
             thumbnails = (self.op.thumbnailsInControls === true) ? self.getThumbnailsRoutes() : null;
 
-          self.el.slidesBox.prepend(self.tmpl(self.op.templatesControls, {id: self.el.idSlideBox, slides: self.el.slides, pagesNumber: pagesNumber, numberSimultaneousSlides: self.op.numberSimultaneousSlides, text: self.culture, root: self.op.root, thumbnails: thumbnails}));
+          self.el.slidesBox[op.controlsAdditionOrder](self.tmpl(self.op.templatesControls, {id: self.el.idSlideBox, slides: self.el.slides, pagesNumber: pagesNumber, numberSimultaneousSlides: self.op.numberSimultaneousSlides, text: self.culture, root: self.op.root, thumbnails: thumbnails}));
           self.startSlide();
         },
 
         getControls: function (controls) {
-          var self = this;
           return self.el.slideControlsBox.find(controls);
         },
 
         startSlide: function () {
-          var self = this;
           self.el.slideControlsBox = self.el.slidesBox.find(op.slidesBoxControls).first();
           self.el.slideControls = self.getSlideControls();
 
@@ -234,7 +238,11 @@
           if (!op.autoStart) {
             self.el.slideControls.pauseResume.remove();
           }
+          
+          self.afterStartSlide();
+        },
 
+        afterStartSlide: function () {
           self.eventControlsNextAndPrevious();
           self.eventControlsFixed();
           self.eventControlsPauseResume();
@@ -243,7 +251,6 @@
 
         startInterval: function () {
           if (op.autoStart) {
-            var self = this;
             self.el.interval = setInterval(function () {
               self.changeSlide(1);
             }, self.el.intervalTime);
@@ -251,7 +258,6 @@
         },
 
         getSlideControls: function () {
-          var self = this;
           return {
             fixed: self.getControls(op.slidesBoxControlsFixed),
             previous: self.getControls(op.slidesBoxControlsPrev),
@@ -259,17 +265,26 @@
             pauseResume: self.getControls(op.slidesBoxControlsPauseResume)
           };
         },
-        
+
         isVisible: function (element) {
           return $(element).is(':visible');
         },
 
         eventControlsNextAndPrevious: function () {
-          var self = this,
-            slideControlsEvent = 'click.sliceControls',
+          var slideControlsEvent = 'click.sliceControls',
             controlPrevious = self.el.slideControls.previous,
             controlNext = self.el.slideControls.next;
-          if ($.isTouchCapable()) {
+          self.touchControls(controlPrevious, controlNext);
+          controlPrevious.off(slideControlsEvent).on(slideControlsEvent, function (ev) {
+            self.launchChangeSlide(ev, -1);
+          });
+          controlNext.off(slideControlsEvent).on(slideControlsEvent, function (ev) {
+            self.launchChangeSlide(ev, 1);
+          });
+        },
+
+        touchControls: function (controlPrevious, controlNext) {
+          if (_.isFunction($.isTouchCapable) && $.isTouchCapable()) {
             self.el.slides.off('swipe.slides').on('swipe.slides', function (ev, touch) {
               if (touch.direction === 'right' && self.isVisible(controlPrevious)) {
                 self.launchChangeSlide(ev, -1);
@@ -278,38 +293,31 @@
               }
             });
           }
-          controlPrevious.off(slideControlsEvent).on(slideControlsEvent, function (ev) {
-            self.launchChangeSlide(ev, -1);
-          });
-          controlNext.off(slideControlsEvent).on(slideControlsEvent, function (ev) {
-            self.launchChangeSlide(ev, 1);
-          });
         },
+
         launchChangeSlide: function (ev, direction) {
           ev.preventDefault();
-          this.pauseSlide();
-          this.changeSlide(direction);
+          self.pauseSlide();
+          self.changeSlide(direction);
         },
 
         eventControlsFixed: function () {
-          var self = this;
-
           self.el.fixedLinks = self.el.slideControls.fixed.find(op.links);
-
-          self.el.fixedLinks.on('click', function (event) {
-            event.preventDefault();
-            var newSelectedInFixed = $(this).closest(op.slidesBoxControlsFixed),
-              selectedInFixed = newSelectedInFixed.siblings(op.slidesBoxSlideActive);
-            self.pauseSlide();
-            self.goToSlide($(this), $(this).attr(op.attrDestination), newSelectedInFixed, selectedInFixed, 1, true);
-          });
+          self.el.fixedLinks.on('click', self.clickFixedLink);
           self.ariaControlsFixed();
         },
 
+        clickFixedLink: function (ev) {
+          ev.preventDefault();
+          var newSelectedInFixed = $(this).closest(op.slidesBoxControlsFixed),
+            selectedInFixed = newSelectedInFixed.siblings(op.slidesBoxSlideActive);
+          self.pauseSlide();
+          self.goToSlide($(this), $(this).attr(op.attrDestination), newSelectedInFixed, selectedInFixed, 1, true);
+        },
+
         eventControlsPauseResume: function () {
-          var self = this;
-          self.el.slideControls.pauseResume.bind('click', function (event) {
-            event.preventDefault();
+          self.el.slideControls.pauseResume.bind('click', function (ev) {
+            ev.preventDefault();
             if (self.el.slideControls.pauseResume.find(op.slidesBoxControlsStatePlaying).length > 0) {
               self.pauseSlide();
             } else {
@@ -320,19 +328,16 @@
         },
 
         resumeSlide: function () {
-          var self = this;
           self.startInterval();
         },
 
         pauseSlide: function () {
-          var self = this;
           clearInterval(self.el.interval);
           self.el.slideControls.pauseResume.html(self.tmpl(op.templateControlsPaused, {text: self.culture}));
         },
 
         changeSlide: function (direction, focus) {
-          var self = this,
-            newSelectedInFixed,
+          var newSelectedInFixed,
             link,
             destination,
             selectedInFixed = self.el.slideControlsBox.find(op.slidesBoxSlideActive).first();
@@ -374,8 +379,7 @@
           selectedInFixed.removeClass(op.classesActive);
           newSelectedInFixed.addClass(op.classesActive);
 
-          var self = this,
-            activeSlides = $(destination).siblings(op.slidesBoxSlideActive),
+          var activeSlides = $(destination).siblings(op.slidesBoxSlideActive),
             newActiveSlides;
 
           if ($(destination).is(':hidden')) {
@@ -444,16 +448,22 @@
           selectedCulture = op.fallbackCultureJson;
         }
         initSliceSlides(selectedCulture);
-      } else {
+      } else if (!_.isNull(op.templatesCultureUrl)) {
         getCulturesJson();
+      } else {
+        initSliceSlides(null);
       }
     }
 
-    if ($('#' + op.idSliceSlideTemplates).length === 0) {
+    function retrieveTemplates () {
       $.get(op.templatesUrl, function (data) {
         $('body').append('<div id="' + op.idSliceSlideTemplates + '">' + data + '</div>');
         questionCultures();
       });
+    }
+
+    if (op.slideTemplatesPlaceholder && $('#' + op.idSliceSlideTemplates).length === 0) {
+      retrieveTemplates();
     } else {
       questionCultures();
     }
